2e85a2957777ca411e348595b770e8876b931146
BBLUE-1112 <nilkanta@sourcedeskglobal.com>
2025-05-14 16:39:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end("<h1>Hello, World!</h1>");
 });
 
-const PORT = 3000;
+const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
