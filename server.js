3b2d76d5ab89424c61dc4027e3cb7dd06313c57c
Suvayan Mondal <suvayan@sourcedeskglobal.com>
2025-05-14 18:21:47 +0530

    Changes to server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello, Nilkanta!</h1>");
+  res.end("<h1>Hello, Arman!</h1>");
 });
 
 const PORT = 8080;
