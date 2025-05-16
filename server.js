c2d4722cf7b68cea9bbc151913a912038eb173e3
Suvayan Mondal <suvayan@sourcedeskglobal.com>
2025-05-16 20:14:18 +0530

    changed name again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello, Hritesh!</h1>");
+  res.end("<h1>Hello, Suvayan!</h1>");
 });
 
 const PORT = 8080;
